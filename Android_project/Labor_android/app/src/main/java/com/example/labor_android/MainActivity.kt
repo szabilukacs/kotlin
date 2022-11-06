1519311bb5a649864f8f476d025b9fbbe87b93a2
szabilukacs <64462503+szabilukacs@users.noreply.github.com>
2022-11-06 20:17:28 +0200

    startscreen

--- Android_project/Labor_android/app/src/main/java/com/example/labor_android/MainActivity.kt
+++ Android_project/Labor_android/app/src/main/java/com/example/labor_android/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.labor_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.Toast
-import android.content.Intent
-import android.widget.EditText
+import com.example.labor_android.fragment.QuizStartFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,25 +11,9 @@ class MainActivity : AppCompatActivity() {
     private val TAG: String = javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d(TAG,"app started")
+        Log.d(TAG, "app started")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val startButton : Button = findViewById(R.id.start_button_quiz)
-        startButton.setOnClickListener{
-            Log.d(TAG,"Clicked on button")
-            Toast.makeText(applicationContext,"Clicked on the button!",Toast.LENGTH_LONG).show()
-            Log.d(TAG,"Clicked on button")
-            
-            // manifestbe kijavitani
-            val intent = Intent(this, SecondActivity::class.java)
-            val value : EditText = findViewById(R.id.plain_text_input)
-            // To pass any data to next activity
-            intent.putExtra("name_input", value.text.toString())
-            // start your next activity
-            Log.d(TAG,"Start new Activity")
-            startActivity(intent)
-        }
     }
 
     override fun onDestroy() {
--- Android_project/Labor_android/app/src/main/java/com/example/labor_android/fragment/QuizStartFragment.kt
+++ Android_project/Labor_android/app/src/main/java/com/example/labor_android/fragment/QuizStartFragment.kt
@@ -2,24 +2,42 @@ package com.example.labor_android.fragment
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.labor_android.R
+import com.example.labor_android.databinding.FragmentQuizStartBinding
 
-class QuizStartFragment : Fragment(R.layout.fragment_start_quiz) {
-    private val TAG: String = javaClass.simpleName
+class QuizStartFragment : Fragment(R.layout.fragment_quiz_start) {
 
-    fun onCreate(view: View, savedInstanceState: Bundle?) {
+    lateinit var binding : FragmentQuizStartBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentQuizStartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+   private val TAG: String = javaClass.simpleName
+
+    fun onCreateView(view: View, savedInstanceState: Bundle?) {
 
         super.onViewCreated(view, savedInstanceState)
 
         val startButton = view.findViewById<Button>(R.id.start_button_quiz)
 
-        //startButton.setOnClickListener {
-          //  this.findNavController().navigate(R.id.)
-       // }
+        startButton.setOnClickListener{
+            Toast.makeText(getActivity(),"Clicked on the button!",Toast.LENGTH_LONG).show()
+            Log.d(TAG,"Clicked on button")
+
+        }
+
     }
+
 }
