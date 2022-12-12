a9fbe5724cf9149c78b3369c12a48ea1cf7ddaba
szabilukacs <64462503+szabilukacs@users.noreply.github.com>
2022-12-12 18:14:52 +0200

    Add animation

--- Project/android-project-example-develop/project_retrofit.app/src/main/java/com/zoltanlorinczi/project_retrofit/SplashActivity.kt
+++ Project/android-project-example-develop/project_retrofit.app/src/main/java/com/zoltanlorinczi/project_retrofit/SplashActivity.kt
@@ -7,23 +7,41 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import com.zoltanlorinczi.project_retorfit.R
-import com.zoltanlorinczi.project_retorfit.databinding.ActivityMainBinding
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    val TAG: String = javaClass.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Log.d(TAG, "Splash onCreate() called!")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        supportActionBar?.hide()
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
-        Handler().postDelayed(Runnable { //This method will be executed once the timer is over
-            // Start your app main activity
-            val i = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(i)
-            // close this activity
+        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
+        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
+        val backgroundImage: ImageView = findViewById(R.id.SplashScreenImage)
+        val slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide)
+        backgroundImage.startAnimation(slideAnimation)
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
             finish()
-        }, 3000)
-
+        }, 3000) // 3000 is the delayed time in milliseconds.
     }
 }
