d872c35d2be9118b838a8f2d6f694c3d89750417
szabilukacs <64462503+szabilukacs@users.noreply.github.com>
2022-12-14 22:21:18 +0200

    Add main screen

---
+++ Project/android-project-example-develop/project_retrofit.app/src/main/java/com/zoltanlorinczi/project_retrofit/fragment/ActivitiesFragment.kt
@@ -0,0 +1,27 @@
+package com.zoltanlorinczi.project_retrofit.fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.zoltanlorinczi.project_retorfit.R
+import com.zoltanlorinczi.project_retorfit.databinding.FragmentMainBinding
+import com.zoltanlorinczi.project_retrofit.api.ThreeTrackerRepository
+import com.zoltanlorinczi.project_retrofit.viewmodel.LoginViewModel
+import com.zoltanlorinczi.project_retrofit.viewmodel.LoginViewModelFactory
+
+class ActivitiesFragment : Fragment() {
+
+    private val TAG: String = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
--- Project/android-project-example-develop/project_retrofit.app/src/main/java/com/zoltanlorinczi/project_retrofit/fragment/MainFragment.kt
+++ Project/android-project-example-develop/project_retrofit.app/src/main/java/com/zoltanlorinczi/project_retrofit/fragment/MainFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.zoltanlorinczi.project_retorfit.R
 import com.zoltanlorinczi.project_retorfit.databinding.FragmentMainBinding
 
@@ -15,7 +17,8 @@ class MainFragment : Fragment() {
     private val TAG: String = javaClass.simpleName
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
@@ -24,6 +27,29 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Log.d(TAG, "hellocska")
+
+        val button1: Button = binding.buttonActivities
+        val button2: Button = binding.buttonMyTasks
+        val button3: Button = binding.buttonGroups
+        val button4: Button = binding.buttonProfile
+
+        Log.d(TAG, button1.text.toString())
+
+        button1.setOnClickListener {
+            Log.d(TAG, "hello")
+            findNavController().navigate(R.id.activitiesFragment)
+        }
+        button2.setOnClickListener {
+            findNavController().navigate(R.id.listFragment)
+        }
+        button3.setOnClickListener {
+            findNavController().navigate(R.id.groupFragment)
+        }
+        button4.setOnClickListener {
+            findNavController().navigate(R.id.myProfileFragment)
+        }
     }
 
 }
